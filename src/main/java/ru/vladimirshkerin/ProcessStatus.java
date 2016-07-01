62e28515fb3fdf61132d597d27ef9076ddd7eafa
Vladimir Shkerin <vladshkerin@gmail.com>
2016-07-01 13:45:21 +0500

    Add enumeration of statuses of a process the operating system "ProcessStatus.java".

---
+++ src/main/java/ru/vladimirshkerin/ProcessStatus.java
@@ -0,0 +1,14 @@
+package ru.vladimirshkerin;
+
+/**
+ * Enumeration of statuses of a process the operating system.
+ *
+ * @author Vladimir Shkerin
+ * @since 30.06.2016
+ */
+public enum ProcessStatus {
+    RUN,
+    RUNNING,
+    WAITING,
+    STOPPED
+}
