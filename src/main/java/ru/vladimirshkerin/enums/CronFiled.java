188b00fa55a3378e2f128c57752218a2235b0f6d
Vladimir Shkerin <vladshkerin@gmail.com>
2016-07-04 15:34:42 +0500

    Add "CronFiled" enumeration corn expression fields.

---
+++ src/main/java/ru/vladimirshkerin/enums/CronFiled.java
@@ -0,0 +1,16 @@
+package ru.vladimirshkerin.enums;
+
+/**
+ * Enumeration corn expression fields.
+ *
+ * @author Vladimir Shkerin
+ * @since 02.07.2016
+ */
+public enum CronFiled {
+    SECONDS,
+    MINUTES,
+    HOURS,
+    DAY_OF_MONTH,
+    MONTH,
+    DAY_OF_WEEK
+}
